5ee9b4e6e732f1d8af4a779c9d8a78c25042482d
retnowati3099 <retnowati3099@gmail.com>
2023-01-21 11:39:00 +0700

    update

--- hari-1/kasus/exercise_looping_a_triangle.js
+++ hari-1/kasus/exercise_looping_a_triangle.js
@@ -1,5 +1,22 @@
-let hash = "#";
+// menggunakan 1 for loop
+let hash1 = "#";
 for (let i = 0; i < 7; i++) {
-    console.log(hash);
-    hash += "#";
+    console.log(hash1);
+    hash1 += "#";
+}
+
+// menggunakan 1 for loop
+let hash2 = "";
+for (let i = 0; i < 7; i++) {
+    hash2 += "#";
+    console.log(hash2);
+}
+
+// menggunakan nested for
+for (let i = 1; i <= 7; i++) {
+    let hash3 = "";
+    for (let j = 1; j <= i; j++) {
+        hash3 += "#";
+    }
+    console.log(hash3);
 }
