1810fd18f61adce83d07a93e4354c26e70f0b19a
retnowati3099 <retnowati3099@gmail.com>
2023-01-24 10:32:23 +0700

    Update file

--- minggu-2/hari-1/latihan/exercise.js
+++ minggu-2/hari-1/latihan/exercise.js
@@ -1,33 +1,44 @@
-// Create an object called shape that has the type property and a getType() method.
-// Define a Triangle() constructor function whose prototype is shape. Objects created with Triangle() should have three own properties—a, b, and c, representing the lengths of the sides of a triangle.
-// Add a new method to the prototype called getPerimeter().
-// Test your implementation with the following code:
+// 1. Create an object called shape that has the type property and a getType() method.
+let shape = {
+    type: " ",
+    getType: function(){
+        return this.type;
+    }
+};
 
-// > var t = new Triangle(1, 2, 3);
-// > t.constructor === Triangle;
-       
-// true
-// > shape.isPrototypeOf(t);
-//         true
-// > t.getPerimeter();
-//        6
-// > t.getType();
-       
-// "triangle"
+// 2. Define a Triangle() constructor function whose prototype is shape. Objects created with Triangle() should have three own properties—a, b, and c, representing the lengths of the sides of a triangle.
+function Triangle(x, y, z){
+    this.type = "triangle";
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+Triangle.prototype = shape;
 
-// Loop over t showing only own properties and methods (none of the prototype's).
-// Make the following code work:
-
-// > [1, 2, 3, 4, 5, 6, 7, 8, 9].shuffle();     
-// [2, 4, 1, 8, 9, 6, 5, 3, 7]
-
-
-// Define a Triangle() constructor function
-function Triangle(a, b, c){
-    this.a = a;
-    this.b = b;
-    this.c = c;
+// 3. Add a new method to the prototype called getPerimeter().
+Triangle.prototype.getPerimeter = function (){
+    return this.x + this.y + this.z
 }
 
-let shape = new Triangle();
-shape.prototype.getType
+// constructor didefinisikan sebagai prototype karena setiap object Triangle  
+// harus mengetahui constructor functionnya, tetapi properti ini akan menjadi 
+// sama untuk setiap instance dari Triangle
+Triangle.prototype.constructor = Triangle;
+
+// 4. Test my implementation
+var t = new Triangle(1, 2, 3);
+console.log(t.constructor === Triangle); // output: true
+console.log(shape.isPrototypeOf(t)); // output: true
+console.log(t.getPerimeter()) // output: 6
+console.log(t.getType()); // output: triangle
+
+// 5. Loop over t showing only own properties and methods (none of the prototype's)
+for(key_value of Object.entries(t)){
+    console.log(key_value);
+}
+
+// 6. Make the following code work
+// Make the following code work:
+// [1, 2, 3, 4, 5, 6, 7, 8, 9].shuffle();       
+// [2, 4, 1, 8, 9, 6, 5, 3, 7]
+console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].shuffle());
